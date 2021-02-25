a8e7a1299f641ab0452a246aa188039fcf3fc3c5
JBetoReyes <josereyes55@hotmail.com>
2021-02-24 22:29:17 -0600

    adding a new entry while booting the app

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,16 +1,25 @@
 package tacos;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table
-public class Ingredient {
+public class Ingredient implements Persistable<String> {
     @Id
     private final String id;
     private final String name;
     private final Type type;
+
+    @Override
+    public boolean isNew() {
+        return true;
+    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -1,13 +1,23 @@
 package tacos;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tacos.data.IngredientRepository;
 
 @SpringBootApplication
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner dataLoader(IngredientRepository repo) {
+		return args -> {
+			repo.save(new Ingredient("OATO", "OAT Tortilla", Ingredient.Type.WRAP));
+		};
 	}
 
 }
